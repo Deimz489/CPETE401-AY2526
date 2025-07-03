9a7396c890522685980181fd61a0b72bed37d256
Deimz489 <s2010708@usls.edu.ph>
2025-07-03 11:23:58 +0800

    Update ACT001.sql

--- ACT001.sql
+++ ACT001.sql
@@ -1,1 +1,62 @@
+-- problem 1
+CREATE TABLE students (
+    LNAME VARCHAR (100), 
+    FNAME VARCHAR(100)
+    );
 
+CREATE TABLE authors(
+    author_id INT,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    biography VARCHAR(100),
+    date_birth DATE
+    );
+CREATE TABLE publishers(
+    publisher_id INT,
+    full_name VARCHAR(100),
+    country_origin VARCHAR (100)
+    );
+
+CREATE TABLE books(
+    book_id INT,
+    book_title VARCHAR(100),
+    publication_date DATE,
+    price DECIMAL (10,2)
+    );
+
+
+-- problem 2
+
+CREATE TABLE patients(
+    patient_id INT,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    patient_dob DATE
+    );
+
+CREATE TABLE doctors(
+    doctor_id INT,
+    first_name VARCHAR(100),
+    last_name VARCHAR(100),
+    medical_specialty ENUM('Cardiology','Pediatrics','Surgery')
+    );
+
+CREATE TABLE appointements(
+    appointement_id INT,
+    start_time DATETIME,
+    reason_detail TEXT,
+    appointement_status ENUM('Seheduled', 'Completed', 'Canceled', 'No-Show') NOT NULL
+    );
+
+-- problem 3
+CREATE TABLE departments(
+    department_id INT,
+    department_name VARCHAR(100),
+    department_office VARCHAR(100)
+    );
+
+CREATE TABLE courses(
+    course_id INT,
+    course_code VARCHAR(255),
+    
+    );
